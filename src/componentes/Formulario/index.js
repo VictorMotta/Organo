55c5035db2b858dcc37be23a0dfffea633849468
Victor Motta <victor.mottag@gmail.com>
2022-11-10 15:13:15 -0300

    Fazendo o formulário criar um objeto

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,28 +1,57 @@
+import { useState } from "react";
 import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
-const Formulario = () => {
-  const times = ["Gamelanders", "Furia", "VivoKeyd", "Team Vikings", "LOUD", "Liberty"];
+const Formulario = (props) => {
+  const times = ["", "Gamelanders", "Furia", "VivoKeyd", "Team Vikings", "LOUD", "Liberty"];
+
+  const [nome, setNome] = useState("");
+  const [main, setMain] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
 
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log("Form foi Submetido");
+    props.aoColaboradorCadastrado({
+      nome,
+      main,
+      imagem,
+      time,
+    });
   };
 
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do jogador.</h2>
-        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+        />
         <CampoTexto
           obrigatorio={true}
           label="Personagem Principal"
           placeholder="Digite seu(a) personagem principal"
+          valor={main}
+          aoAlterado={(valor) => setMain(valor)}
         />
-        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-        <ListaSuspensa required={true} label="Time" itens={times} />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Informe o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+        <ListaSuspensa
+          label="Time"
+          itens={times}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
         <Botao>Criar card</Botao>
       </form>
     </section>
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,10 +1,14 @@
 import "./ListaSuspensa.css";
 
 const ListaSuspensa = (props) => {
+  const aoAlterar = (evento) => {
+    props.aoAlterado(evento.target.value);
+  };
+
   return (
     <div className="lista-suspensa">
       <label>{props.label}</label>
-      <select required={props.required}>
+      <select value={props.valor} onChange={aoAlterar} required={props.required}>
         {props.itens.map((item) => {
           return <option key={item}>{item}</option>;
         })}
